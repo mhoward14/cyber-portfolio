7cedceddb8689e44e2b1d8490bdcb86619b4eafc
Matthew Howard <32048379+mhoward14@users.noreply.github.com>
2026-04-08 17:00:13 -0500

    Final production build

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,10 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(), provideClientHydration(withEventReplay()),
-    
+    // This tells Angular: "I'm using Zone.js, please bundle my events together"
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideClientHydration(withEventReplay())
   ]
 };
